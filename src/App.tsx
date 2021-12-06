8251dbd7ce2513c256e743e939701a8703f10c4d
William <sebastiani.1753672@studenti.uniroma1.it>
2021-12-06 19:29:39 +0100

    react template starter

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRoutes } from 'react-router-dom';
+import routes from './router';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
 
+import ThemeProvider from './theme/ThemeProvider';
+import { CssBaseline } from '@mui/material';
 
-function App() {
+const App = () => {
+
+  const content = useRoutes(routes);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <CssBaseline />
+        {content}
+      </LocalizationProvider>
+    </ThemeProvider>
   );
 }
-
 export default App;
